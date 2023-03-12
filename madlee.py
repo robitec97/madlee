5c1ed345dbf24ecd28bc3a163ed1253dcbaca7f0
robitec97 <45318540+robitec97@users.noreply.github.com>
2023-03-12 15:38:36 +0100

    small fixes

--- madlee.py
+++ madlee.py
@@ -1,14 +1,14 @@
 import transformers
+from diffusers import DiffusionPipeline
+import torch
 
 def startup():
-    from diffusers import DiffusionPipeline
-    import torch
     pipeline = DiffusionPipeline.from_pretrained("dreamlike-art/dreamlike-photoreal-2.0",torch_dtype=torch.float16,safety_checker=None)
     pipeline.to("cuda")
     return pipeline
 
 def generate(pipeline):
-    prompt = input("Mad-lee: Inserisci un prompt per iniziare la generazione")
+    prompt = input("Mad-lee: Inserisci un prompt per iniziare la generazione ")
     image = pipeline(prompt).images[0]
     from IPython.display import display,Image
     image.save("generated.png",format="PNG",quality=100)
